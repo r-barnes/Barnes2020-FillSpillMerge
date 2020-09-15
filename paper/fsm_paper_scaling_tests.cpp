1f6e0dd3ce300fae9682ca20609f82c9e53992bf
Richard Barnes <rbarnes@umn.edu>
2020-09-15 09:13:48 -0600

    Update scaling tests

--- paper/fsm_paper_scaling_tests.cpp
+++ paper/fsm_paper_scaling_tests.cpp
@@ -1,64 +1,68 @@
-#include <richdem/terrain_generation.hpp>
-#include <richdem/common/loaders.hpp>
-#include <fsm/fill_spill_merge.hpp>
 #include <dephier/dephier.hpp>
+#include <fsm/fill_spill_merge.hpp>
+#include <richdem/common/loaders.hpp>
+#include <richdem/terrain_generation.hpp>
 
-namespace rd = richdem;
-namespace dh = richdem::dephier;
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <vector>
 
-/**
-  @brief Pads a 2D array on all sides by cells of a given value
+using namespace richdem;
+using namespace richdem::dephier;
 
-  @param[in] pad_width Positive number of cells in the padding
-  @param[in] value     Value to give to the padding cells
+const double ocean_level = -1;
 
-  @return A new array based on the old one
-*/
-rd::Array2D<double> pad(const rd::Array2D<double> &inp, const int pad_width, const double value){
-  rd::Array2D<double> temp(inp.width()+2*pad_width, inp.height()+2*pad_width);
 
-  temp.setAll(value);
 
-  if(pad_width<0)
-    throw std::runtime_error("Cannot have a negative padding!");
+void time_fsm(std::mt19937_64 &gen, const int size){
+  static std::uniform_int_distribution<uint32_t> seed_dist;
 
-  for(size_t y=0;y<inp.height();y++)
-  for(size_t x=0;x<inp.width();x++){
-    temp(x+pad_width,y+pad_width) = inp(x,y);
+  auto dem = perlin(size, seed_dist(gen));
+
+  Array2D<double>     wtd     (dem.width(),dem.height(), 10);
+  Array2D<dh_label_t> labels  (dem.width(),dem.height(), NO_DEP);
+  Array2D<flowdir_t>  flowdirs(dem.width(),dem.height(), NO_FLOW);
+
+  for(int y=0;y<dem.height();y++)
+  for(int x=0;x<dem.width(); x++){
+    if(dem.isEdgeCell(x,y)){
+      dem(x,y)    = ocean_level;
+      labels(x,y) = OCEAN;
+    }
   }
 
-  return temp;
+  Timer time_dh, time_fsm, time_both;
+
+  time_both.start();
+  time_dh.start();
+  auto deps = GetDepressionHierarchy<double,Topology::D8>(dem, labels, flowdirs);
+  time_dh.stop();
+
+  time_fsm.start();
+  FillSpillMerge<double,double>(dem, labels, flowdirs, deps, wtd);
+  time_fsm.stop();
+  time_both.stop();
+
+  std::cout<<"DH,  "<<std::setw(5)<<size<<","<<time_dh.accumulated()<<std::endl;
+  std::cout<<"FSM, "<<std::setw(5)<<size<<","<<time_fsm.accumulated()<<std::endl;
+  std::cout<<"Both,"<<std::setw(5)<<size<<","<<time_both.accumulated()<<std::endl;
 }
 
 
 
 int main(){
-  auto dem = richdem::perlin(2000, 123456);
+  std::mt19937_64 gen;
 
-  const double ocean_level = -1;
+  //Sizes to test at
+  const std::vector<int> sizes = {10,50,100,500,1000,5000,10000,50000};
 
-  dem = pad(dem, 1, ocean_level);
+  //Number of times to run each test
+  const int reps = 3;
 
-  //Add water everywhere. Since elevation of the random terrain is in the range
-  //[0,1], this is actually quite a bit.
+  for(const auto size: sizes)
+  for(int i=0;i<reps;i++)
+    time_fsm(gen, size);
 
-  rd::Array2D<double>         wtd     (dem.width(),dem.height(), 10);
-  rd::Array2D<dh::dh_label_t> labels  (dem.width(),dem.height(), dh::NO_DEP);
-  rd::Array2D<rd::flowdir_t>  flowdirs(dem.width(),dem.height(), rd::NO_FLOW);
-
-  labels.setAll(dh::NO_DEP);
-  for(int y=0;y<labels.height();y++)
-  for(int x=0;x<labels.width() ;x++){
-    if(labels.isEdgeCell(x,y)){
-      labels(x,y) = dh::OCEAN;
-      wtd(x,y)    = 0;
-    }
-  }
-
-  flowdirs.setAll(rd::NO_FLOW);
-
-  auto deps = dh::GetDepressionHierarchy<double,rd::Topology::D8>(dem, labels, flowdirs);
-  dh::FillSpillMerge<double,double>(dem, labels, flowdirs, deps, wtd);
-
-  SaveGDAL(dem, "/z/out.tif");
+  return 0;
 }
