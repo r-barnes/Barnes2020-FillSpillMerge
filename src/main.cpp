983734c777f6aa00288f3697b2cae0e8a35bd0ce
Richard Barnes <rbarnes@umn.edu>
2020-09-16 11:39:00 -0600

    Clean up main.cpp/fsm.exe and add doc about it to readme

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <fsm/fill_spill_merge.hpp>
 #include <richdem/common/Array2D.hpp>
 #include <richdem/misc/misc_methods.hpp>
+#include <richdem/common/gdal.hpp>
 
 #include <iostream>
 #include <string>
@@ -11,6 +12,7 @@ namespace dh = richdem::dephier;
 
 int main(int argc, char **argv){
   if(argc!=5){
+    std::cout<<"Pours a given amount of water onto every cell of a landscape and determines where it all ends up\n"<<std::endl;
     std::cout<<"Syntax: "<<argv[0]<<" <Input> <Output> <Surface Water Level> <Ocean Level>"<<std::endl;
     std::cout<<"Ocean cells are detected at the DEM perimeter and added from there"<<std::endl;
     return -1;
@@ -21,30 +23,31 @@ int main(int argc, char **argv){
   const double      surf_water_level = std::stod(argv[3]);
   const double      ocean_level      = std::stod(argv[4]);
 
-  std::cout<<"m Processing          = "<<argv[1]<<std::endl;
+  std::cout<<"m Input DEM           = "<<argv[1]<<std::endl;
   std::cout<<"m Output prefix       = "<<argv[2]<<std::endl;
   std::cout<<"m Surface water level = "<<argv[3]<<std::endl;
   std::cout<<"m Ocean level         = "<<argv[4]<<std::endl;
 
   rd::Timer timer_io;
   timer_io.start();
-  rd::Array2D<float> topo(in_name);   //Recharge (Percipitation minus Evapotranspiration)
+  rd::Array2D<double> topo(in_name);
   timer_io.stop();
 
   std::cout<<"m Data width  = "<<topo.width ()<<std::endl;
   std::cout<<"m Data height = "<<topo.height()<<std::endl;
   std::cout<<"m Data cells  = "<<topo.numDataCells()<<std::endl;
 
-  rd::Array2D<float>          wtd     (topo.width(), topo.height(), surf_water_level); //All cells have some water
-  rd::Array2D<dh::dh_label_t> label   (topo.width(), topo.height(), dh::NO_DEP ); //No cells are part of a depression
-  rd::Array2D<rd::flowdir_t>  flowdirs(topo.width(), topo.height(), rd::NO_FLOW); //No cells flow anywhere
+  rd::Array2D<double>          wtd     (topo.width(), topo.height(), surf_water_level); //All cells have some water
+  rd::Array2D<dh::dh_label_t> label   (topo.width(), topo.height(), dh::NO_DEP );      //No cells are part of a depression
+  rd::Array2D<rd::flowdir_t>  flowdirs(topo.width(), topo.height(), rd::NO_FLOW);      //No cells flow anywhere
 
   wtd.setNoData(topo.noData());
 
-  rd::BucketFillFromEdges<rd::Topology::D8>(topo, label, (float)ocean_level, dh::OCEAN);
-
   //Label the ocean cells. This is a precondition for using
   //`GetDepressionHierarchy()`.
+  rd::BucketFillFromEdges<rd::Topology::D8>(topo, label, ocean_level, dh::OCEAN);
+
+  //Make NoData cells also ocean cells. Ocean has no water on it to begin with.
   #pragma omp parallel for
   for(unsigned int i=0;i<label.size();i++){
     if(topo.isNoData(i) || label(i)==dh::OCEAN){
@@ -53,28 +56,34 @@ int main(int argc, char **argv){
     }
   }
 
+  rd::Timer timer_calc;
+  timer_calc.start();
+
   //Generate flow directions, label all the depressions, and get the hierarchy
   //connecting them
-  auto deps = dh::GetDepressionHierarchy<float,rd::Topology::D8>(topo, label, flowdirs);
+  auto deps = dh::GetDepressionHierarchy<double,rd::Topology::D8>(topo, label, flowdirs);
 
   dh::FillSpillMerge(topo, label, flowdirs, deps, wtd);
+
+  timer_calc.stop();
+
+  timer_io.start();
+
+  //Output the water table depth
+  wtd.saveGDAL(out_name+"-wtd.nc");
 
   for(unsigned int i=0;i<topo.size();i++)
     if(!topo.isNoData(i))
       wtd(i) += topo(i);
 
-  // SaveAsNetCDF(wtd,out_name+"-flooded.nc","value");
+  //Output the new height of the hydraulic surface
+  wtd.saveGDAL(out_name+"-hydraulic-surface-height.nc");
 
-  // rd::FillDepressions<rd::Topology::D8>(topo);
-  // SaveAsNetCDF(topo,out_name+"-filled.nc","value");
+  timer_io.stop();
 
-  // rd::Array2D<float> diff(wtd);
-  // for(unsigned int i=0;i<topo.size();i++)
-  //   diff(i) = wtd(i)-topo(i);
-  // SaveAsNetCDF(diff,out_name+"-diff.nc","value");
-
-  // std::cout<<"Finished"<<std::endl;
-  // std::cout<<"IO time   = "<<timer_io.accumulated()<<" s"<<std::endl;
+  std::cout<<"Finished."<<std::endl;
+  std::cout<<"IO time   = "<<timer_io.accumulated()  <<" s"<<std::endl;
+  std::cout<<"Calc time = "<<timer_calc.accumulated()<<" s"<<std::endl;
 
   return 0;
 }
